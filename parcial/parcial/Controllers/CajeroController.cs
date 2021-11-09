dd8d58192e3f2c6a5b55ddadece9bd38a047c628
Wenpineda <cp0428032021@unab.edu.sv>
2021-11-09 09:51:29 -0600

    TwT no me funciona completo

---
+++ parcial/parcial/Controllers/CajeroController.cs
@@ -0,0 +1,49 @@
+﻿using parcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace parcial.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult IndexVista()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult correcto()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult error()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult IndexVista(cajero c)
+        {
+
+            if (c.dato % 5 == 0)
+            {
+                return Redirect("correcto");
+            }
+            else
+            {
+                return Redirect("Error");
+            }
+        }
+    }
+}
